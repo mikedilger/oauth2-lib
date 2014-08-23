1926d7d776904c2bfbcbbbe31856ea31b368fc22
Mike Dilger <mike@mikedilger.com>
2014-08-24 11:34:56 +1200

    Cargo.toml and lib.rs started

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+/*!
+This is an OAuth 2.0 library for the Rust language.  It defines types and traits useful
+for implementing an OAuth 2.0 compliant system as specified by RFC 6749.
+*/
+
+#![experimental]
